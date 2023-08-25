862444c4c54709544e45abe263f2233dba6382ca
LeoD3v <leoargusa1991@gmail.com>
2023-08-25 20:03:07 +0200

    Completed lab

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+
+    Recipe.deleteMany().then(() => {
+      
+    })
+    // return Recipe.deleteMany();
+  })
+
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then((data) => {
+    console.log(`${data} recipe inserted`);
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then((result) => {
+    if (result.deletedCount > 0) {
+      console.log(`Carrot Cake was deleted`);
+    } else console.log("Carrot Cake was NOT deleted");
+  })
+  .catch((err) => {
+    console.log(`Data was NOT inserted ${err}`);
+  })
+
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+      // { new: true },
+      // (error, result) => {
+      //   if (error) {
+      //     console.log(`Recipe duration did not update: ${error}`);
+      //   } else console.log(`Recipe duration was UPDATED!${result}`);
+      // }
+    ).exec();
+  })
+  .then((update) => {
+    console.log(`the updated object:${update} was successful`);
+  })
+  .catch((err) => {
+    console.log(err);
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+    mongoose.connection.close(() => {
+      console.log("Mongoose connection disconnected !");
+    });
   });
